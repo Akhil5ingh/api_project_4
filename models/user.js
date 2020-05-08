ba3539329bb3dc84d6e535b31e4976d7795a2511
Akhil Singh <47193522+Akhil5ingh@users.noreply.github.com>
2020-05-08 20:36:55 +0530

    Add files via upload

--- models/user.js
+++ models/user.js
@@ -1,13 +1,23 @@
-const mongoose = require('mongoose');
+// grab the things we need
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-const Schema = mongoose.Schema;
-
-const userSchema = new Schema({
-  username: { type: String, required: true, unique: true },
-}, {
-  timestamps: true,
+// create a schema
+var exerciseSchema = new Schema({
+  "description":{type: String},
+  "duration":{type:Number},
+  "date":{type: Date}
 });
 
-const User = mongoose.model('User', userSchema);
+var userSchema = new Schema({
+  "username": {type: String, required:true},
+  "_id":{type:String, requried:true},
+  "exercise": [exerciseSchema]
+});
 
+// the schema is useless so far
+// we need to create a model using it
+var User = mongoose.model('User',userSchema);
+
+// make this available to our users in our Node applications
 module.exports = User;
--- server.js
+++ server.js
@@ -1,185 +1,221 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const path = require('path');
-require('dotenv').config();
-const User = require('./models/user');
-const Exercise = require('./models/exercise');
+'use strict';
 
-const app = express();
-const port = process.env.PORT;
-const mongoDB = process.env.MLAB_URI;
+// require('dotenv').load();
 
-mongoose.connect(mongoDB);
-const db = mongoose.connection;
-db.on('error', (err) => { console.log('Mongo DB connection error', err); });
-db.once('open', () => { console.log('Mongo DB connected.'); });
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
 
-app.use(cors({ optionSuccessStatus: 200 }));  // some legacy browsers choke on 204
+const cors = require('cors')
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const mongoose = require('mongoose')
 
-app.use(express.static('public'));
+mongoose.connect(process.env.MONGOLAB_URI,
+  {useNewUrlParser: true},
+  function(error){
+    if(error) console.log(error);
+    console.log("connection successful");
+});
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+
+
+app.use(cors())
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+
+app.use(express.static('public'))
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/index.html'));
+  res.sendFile(__dirname + '/views/index.html')
 });
 
-// Create new User route
-app.post('/api/exercise/new-user/', (req, res) => {
-  const username = req.body.username;
 
-  if (username === '') {
-    res.send('Username cannot be blank');
-  } else if (username.length > 10) {
-    res.send('Username cannot be greater than 10 characters');
-  } else {
-    const newUser = new User({
-      username,
-    });
 
-    newUser.save((err, data) => {
-      if (err) {
-        if (err.name === 'MongoError' && err.code === 11000) { // Duplicate key error
-          res.send('Duplicate username, try a different username');
-        } else {
-          res.send('Errpr pccurred while saving user');
-        }
-      } else {
-        res.json(data);
-      }
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
+
+// My code
+var User = require('./models/user');
+
+// To clean records
+// User.deleteMany({}, function(err, users) {
+//   if (err) throw err;
+//   console.log('User: Remove old records');
+// });
+
+// Find records
+var findUsername = function(res,name){
+  User.find({"username": name}, (err,data)=>{
+    if (err) { return console.log("Error: find user name")}
+    if (data.length===0){
+      newIdAndCreate(res,name)
+    } else {
+      console.log("Find old record, id: ",data[0]["_id"])
+      res.send('username already taken')
+    }
+  })
+}
+
+var newIdAndCreate = function(res,name){
+  User.countDocuments({}, function(err, count) {
+    if (err) { return console.log("Error: coundDocuments") }
+    var id = ('0000'+(count+1)).slice(-5)
+    console.log('New id: ', id)
+    createAndSaveUser(name,id)
+    res.json({
+      "username": name,
+      "_id": id
     });
+  });
+}
+
+var createAndSaveUser = function(name,id){
+  var newUser = new User({
+    "username": name,
+    "_id": id
+  });
+  newUser.save(function(err){
+    if (err) { return console.log("Error: save user")};
+    console.log('New user created, id: ',id)
+    return
+  });
+};
+
+// For adding exercise
+var findUserId = function(req,res){
+  User.find({"_id": req.body.userId}, (err,data)=>{
+    if (err) { return console.log("Error: find user name")}
+    if (data.length===0){
+      res.send('unknown _id')
+    } else {
+      pushExer(req,res,data)
+    }
+  })
+}
+
+var pushExer = function(req,res,data){
+  var id= req.body.userId;
+  var description= req.body.description;
+  var duration= req.body.duration;
+
+  if (description=='') { return res.send('Path `description` is required.')}
+  if (duration=='') { return res.send('Path `duration` is required.')}
+  if (parseInt(duration).toString()!=duration) {
+    console.log(duration, parseInt(duration).toString())
+    return res.send('Cast to Number failed for value "'+duration+'" at path "duration"')
   }
-});
 
-// Create new Exercise route
-app.post('/api/exercise/add', (req, res) => {
-  const username = req.body.username;
-  const description = req.body.description;
-  let duration = req.body.duration;
-  let date = req.body.date;
-  let userId;
-
-  if (username === undefined || description === undefined || duration === undefined) {
-    res.send('Required Field(s) are missing.');
-  } else if (username === '' || description === '' || duration === '') {
-    res.send('Required Field(s) are blank.');
-  } else if (username.length > 10) {
-    res.send('Username cannot be greater than 10 characters');
-  } else if (description.length > 100) {
-    res.send('Description cannot be greater than 100 characters');
-  } else if (isNaN(duration)) {
-    res.send('Duration must be a number');
-  } else if (Number(duration) > 1440) {
-    res.send('Duration must be less than 1440 minutes (24 hours)');
-  } else if (date !== '' && isNaN(Date.parse(date)) === true) {
-    res.send('Date is not a valid date');
+  if (req.body.date==''){
+    var date = new Date();
   } else {
-    // Find userId for username
-    User.findOne({ username }, (err, user) => {
-      if (err) {
-        res.send('Error while searching for username, try again');
-      } else if (!user) {
-        res.send('Username not found');
-      } else {
-        userId = user.id;
-
-        // Valiidations passed, convert duration
-        duration = Number(duration);
-
-        // Valiidations passed, convert date
-        if (date === '') {
-          date = new Date();
-        } else {
-          date = Date.parse(date);
-        }
-
-        const newExercise = new Exercise({
-          userId,
-          description,
-          duration,
-          date,
-        });
-
-        newExercise.save((errSave, data) => {
-          if (errSave) {
-            res.send('Error occurred while saving exercise');
-          } else {
-            res.json(data);
-          }
-        });
-      }
-    });
+    var date = new Date(req.body.date);
+    if (date.getTime() !== date.getTime()){
+      return res.send('Cast to Date failed for value " '+req.body.date+'" at path "date"')
+    }
+    console.log(date)
   }
+
+  var exerData = {
+    "description":description,
+    "duration":   parseInt(duration),
+    "date":       date}
+
+  console.log(exerData)
+
+  var returnData = {
+    "username":   data[0]['username'],
+    "description":description,
+    "duration":   parseInt(duration),
+    "_id":        id,
+    "date":       date.toString().slice(0,15)}
+
+  console.log(returnData)
+
+  User.updateOne({"_id":id},{$push:{"exercise":exerData}},(err)=>{
+    if (err) { return console.log(err,"Error: save exercise")};
+    console.log('New exer created')
+    res.json(returnData);
+  });
+};
+
+app.post('/api/exercise/new-user',(req,res)=>{
+  var name= req.body.username;
+  findUsername(res,name)
 });
 
-// Read Exercise route
-app.get('/api/exercise/:log', (req, res) => {
-  const username = req.query.username;
-  let from = req.query.from;
-  let to = req.query.to;
-  let limit = req.query.limit;
-  let userId;
-  const query = {};
+app.post('/api/exercise/add',(req,res)=>{
+  findUserId(req,res)
+});
 
-  // console.log(req.query);
+app.get('/api/exercise/log',(req,res)=>{
+  var id = req.query.userId
+  User.find({'_id':id},(err,data)=>{
+    if (err) { return console.log(err,'Error:find records from query')}
+    var result = {
+      "_id": data[0]["_id"],
+      "username": data[0]["username"]
+    }
+    var log = data[0]["exercise"]
+    if (req.query.from!=undefined){
+      var from = new Date(req.query.from)
+      result["from"]=from.toString().slice(0,15)
+      log = log.filter((x) => x['date'] > from)
+    };
+    if (req.query.to!=undefined){
+      var to = new Date(req.query.to)
+      result["to"]=to.toString().slice(0,15)
+      log = log.filter((x) => x['date'] < to)
+    };
+    if (req.query.limit!=undefined){
+      var limit = req.query.limit
+      if (log.length > limit){
+        log = log.slice(0,limit)
+      }
+    };
+    result["count"] = log.length;
+    log = log.map((x)=>{
+      return {
+        "description": x["description"],
+        "duration": x["duration"],
+        "date": x["date"].toString().slice(0,15)
+      }
+    })
+    result['log'] = log;
+    res.json(result)
+  })
+});
 
-  if (username === undefined) {
-    res.send('Required Field(s) are missing.');
-  } else if (username === '') {
-    res.send('Required Field(s) are blank.');
-  } else if (username.length > 10) {
-    res.send('Username cannot be greater than 10 characters');
-  } else if (from !== undefined && isNaN(Date.parse(from)) === true) {
-    res.send('From is not a valid date');
-  } else if (to !== undefined && isNaN(Date.parse(to)) === true) {
-    res.send('From is not a valid date');
-  } else if (limit !== undefined && isNaN(limit) === true) {
-    res.send('Limit is not a valid number');
-  } else if (limit !== undefined && Number(limit) < 1) {
-    res.send('Limit must be greater than 0');
+
+// End of my own code
+
+// Error handling
+
+// Not found middleware
+app.use((req, res, next) => {
+  return next({status: 404, message: 'not found'})
+})
+
+// Error Handling middleware
+app.use((err, req, res, next) => {
+  let errCode, errMessage
+
+  if (err.errors) {
+    // mongoose validation error
+    errCode = 400 // bad request
+    const keys = Object.keys(err.errors)
+    // report the first validation error
+    errMessage = err.errors[keys[0]].message
   } else {
-    // Find userId for username\
-    User.findOne({ username }, (err, user) => {
-      if (err) {
-        res.send('Error while searching for username, try again');
-      } else if (!user) {
-        res.send('Username not found');
-      } else {
-        userId = user.id;
-        query.userId = userId;
-
-        if (from !== undefined) {
-          from = new Date(from);
-          query.date = { $gte: from};
-        }
-
-        if (to !== undefined) {
-          to = new Date(to);
-          to.setDate(to.getDate() + 1); // Add 1 day to include date
-          query.date = { $lt: to};
-        }
-
-        if (limit !== undefined) {
-          limit = Number(limit);
-        }
-
-        Exercise.find(query).select('userId description date duration ').limit(limit).exec((errExercise, exercises) => {
-          if (err) {
-            res.send('Error while searching for exercises, try again');
-          } else if (!user) {
-            res.send('Exercises not found');
-          } else {
-            res.json(exercises);
-          }
-        });
-      }
-    });
+    // generic or custom error
+    errCode = err.status || 500
+    errMessage = err.message || 'Internal Server Error'
   }
-});
-
-const listener = app.listen(port, () => {
-  console.log(`Your app is listening on port ${listener.address().port}`);
-});
+  res.status(errCode).type('txt')
+    .send(errMessage)
+})
