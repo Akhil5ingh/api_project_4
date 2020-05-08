226d54fc58e6f11ed5b098a7b50554987e920f57
Akhil Singh <47193522+Akhil5ingh@users.noreply.github.com>
2020-05-08 21:03:23 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,23 +1,13 @@
-// grab the things we need
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
 
-// create a schema
-var exerciseSchema = new Schema({
-  "description":{type: String},
-  "duration":{type:Number},
-  "date":{type: Date}
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+}, {
+  timestamps: true,
 });
 
-var userSchema = new Schema({
-  "username": {type: String, required:true},
-  "_id":{type:String, requried:true},
-  "exercise": [exerciseSchema]
-});
+const User = mongoose.model('User', userSchema);
 
-// the schema is useless so far
-// we need to create a model using it
-var User = mongoose.model('User',userSchema);
-
-// make this available to our users in our Node applications
 module.exports = User;
